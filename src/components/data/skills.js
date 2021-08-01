f886fc257efa607de123a1b17e2e7110aeee0826
Raviranjan <Raviranjan@RAVIRANJAN>
2021-08-01 13:54:17 +0530

    Skills Section Completed

---
+++ src/components/data/skills.js
@@ -0,0 +1,36 @@
+export const SkillsData = [
+  {
+    type: "frontend",
+    list: [
+      {
+        name: "ReactJS",
+        icon: <i class="devicon-react-original colored"></i>,
+      },
+      {
+        name: "Javascript",
+        icon: <i class="devicon-javascript-plain colored"></i>,
+      },
+      {
+        name: "TypeScript",
+        icon: <i class="devicon-typescript-plain colored"></i>,
+      },
+    ],
+  },
+  {
+    type: "backend",
+    list: [
+      {
+        name: ".NET Core",
+        icon: <i class="devicon-dotnetcore-plain colored"></i>,
+      },
+      {
+        name: "AWS",
+        icon: <i class="devicon-amazonwebservices-original colored"></i>,
+      },
+      {
+        name: "Postgres",
+        icon: <i class="devicon-postgresql-plain colored"></i>,
+      },
+    ],
+  },
+];
